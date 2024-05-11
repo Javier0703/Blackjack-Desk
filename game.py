6f5ef292415931bad721ff931d76080ad8cb4979
Javier Calvo <javiercalvoporro@gmail.com>
2024-05-11 20:05:40 +0200

    Mejoras en el wxDialog

--- game.py
+++ game.py
@@ -3,8 +3,15 @@ El código ha sido generado por mí (sin usar wxGlade) ya que me parecía más c
 
 import wx
 from ClasesFunciones import *
+#Creacion de las clases/funciones/variables para la interfaz
+tipoApuesta = [2,10,50]
 
-#Creacion de las clases/funciones para la interfaz
+def obtenerImagenes(images):
+    for i in range(52):
+        img = wx.Bitmap()
+        img.LoadFile(f"Imagenes/Cartas/{i}.png")
+        images.append(img)
+    return images    
 
 #Menu estático izquierdo
 class LateralEstatico(wx.Panel):
@@ -35,9 +42,13 @@ class LateralEstatico(wx.Panel):
         fieldset_accion = wx.StaticBox(self, label="Jugada")
         sizer_accion = wx.StaticBoxSizer(fieldset_accion, wx.VERTICAL)
         self.pedir = wx.Button(self, label="Pedir")
+        self.pedir.Enable(False)
         self.doblar = wx.Button(self, label="Doblar")
+        self.doblar.Enable(False)
         self.cerrar = wx.Button(self, label="Cerrar")
+        self.cerrar.Enable(False)
         self.separar = wx.Button(self, label="Separar")
+        self.separar.Enable(False)
         self.pedir.SetMinSize((0, 25))
         sizer_accion.Add(self.pedir, 1, wx.EXPAND|wx.ALL, 2)
         sizer_accion.Add(self.doblar, 1, wx.EXPAND|wx.ALL, 2)
@@ -97,10 +108,8 @@ class MenuJuego(wx.ScrolledWindow):
 
         self.sizer_menu_juego = wx.BoxSizer(wx.VERTICAL)
         self.SetSizer(self.sizer_menu_juego)
-
         #Paneles donde van las cartas
-        wxPanels = []
-
+        self.wxPanels = []
 
 #Ventana princiapl (juego)
 class Ventana(wx.Frame):
@@ -119,12 +128,53 @@ class Ventana(wx.Frame):
         self.SetSizer(sizer_ventana)
         self.Show(True)
 
+#Dialogo para la seleccion de la apuesta
+class MiDialogo(wx.Dialog):
+    def __init__(self, parent):
+        super(MiDialogo, self).__init__(parent, title="Nueva Partida", size=(300,300))
+        sizer_dialog = wx.BoxSizer(wx.VERTICAL)
+
+        # Generamos las apuestas
+        fieldset = wx.StaticBox(self, label="Elige Apuesta")
+        sizer = wx.StaticBoxSizer(fieldset, wx.VERTICAL)
+        self.baja = wx.RadioButton(self, label=f"{tipoApuesta[0]} €", style=wx.RB_GROUP)
+        self.media = wx.RadioButton(self, label=f"{tipoApuesta[1]} €")
+        self.alta = wx.RadioButton(self, label=f"{tipoApuesta[2]} €")
+        sizer.Add(self.baja, 0, wx.ALL, 5)
+        sizer.Add(self.media, 0, wx.ALL, 5)
+        sizer.Add(self.alta, 0, wx.ALL, 5)
+
+        #Pregunta de que si quiere jugar
+        sizer_pregunta = wx.BoxSizer(wx.VERTICAL)
+        sizer_botones = wx.BoxSizer(wx.HORIZONTAL)
+
+        texto = wx.StaticText(self, label="¿Quieres seguir jugando?", pos=(20, 80))
+        self.si = wx.Button(self, label="Sí", pos=(20, 110))
+        self.no = wx.Button(self, label="No", pos=(80, 110))
+        sizer_botones.Add(self.si ,0, wx.ALL, 5)
+        sizer_botones.Add(self.no,0, wx.ALL, 5)
+        sizer_pregunta.Add(texto, 0, wx.ALL, 5)
+        sizer_pregunta.Add(sizer_botones, 0, wx.ALL, 5)
+
+        #Añadimos al sizer general
+        sizer_dialog.Add(sizer, 0, wx.ALL|wx.EXPAND, 20)
+        sizer_dialog.Add(sizer_pregunta, 0, wx.ALL, 5)
+        self.SetSizer(sizer_dialog)
+        self.Centre()
+        self.SetMinSize((200,200))
+        self.Show(True)
+
+#Comenzamos con la creacion del juego
 def main():
     app = wx.App()
     ventana = Ventana()
     ventana.Show()
+    dialogo = MiDialogo(ventana)  # Pasar la ventana principal como padre del diálogo
+    dialogo.ShowModal() 
     app.MainLoop()
-    return None
+    images = []
+    obtenerImagenes(images)
+
 
 if __name__ == '__main__':
     main()
